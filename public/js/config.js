8a066022b9a82e2afda479a1b60e1605ff84c040
Praneetha <praneetha1506@gmail.com>
2025-12-19 21:36:40 +0530

    Set production API and admin URLs

--- public/js/config.js
+++ public/js/config.js
@@ -1,4 +1,4 @@
 window.PORTFOLIO_CONFIG = {
-  API_BASE_URL: '',
-  ADMIN_PORTAL_URL: ''
+  API_BASE_URL: 'https://portfolio-backend-7q1v.onrender.com',
+  ADMIN_PORTAL_URL: 'https://portfolio-admin-portal-rho.vercel.app'
 };
